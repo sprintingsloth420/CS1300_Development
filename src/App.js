ea9f6415512343a4e4a7cc41917c9c3669ed095a
sprintingsloth420 <115044314+sprintingsloth420@users.noreply.github.com>
2022-11-29 16:57:42 -0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,126 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
+import galleryData from "./assets/gallery-data.json";
+import GalleryItem from './components/GalleryItem.js';
+import Sort from "./components/Sort.js";
+import Filter1 from "./components/Filter1.js";
+import Filter2 from "./components/Filter2.js";
+import Button from '@mui/material/Button';
+import Bookmark from './components/Bookmark';
+
+
+galleryData.forEach((item) => {
+  item.image = process.env.PUBLIC_URL + "/" + item.image;
+});
+
+// renders sort
+function matchesSort({sort}){
+  if(sort === "year"){
+    galleryData = galleryData.sort((a, b) => a.year - b.year);
+  }
+  else if(sort === "dimension"){
+    galleryData = galleryData.sort((a, b) => a.dimension - b.dimension);
+  }
+  else{
+    galleryData = galleryData.sort((a, b) => a.default - b.default);
+  }
+}
+
+// renders filters
+function matchesType({item,type1,type2}){
+  const filter1_noSelect = Object.values(type1).every(val => val === false);
+  const filter2_noSelect = Object.values(type2).every(val => val === false);
+  if (filter1_noSelect && filter2_noSelect){
+    return true;
+  }else{
+    if ((type1[item.media] && type2[item.location]) || (type1[item.media] && filter2_noSelect) || (filter1_noSelect && type2[item.location])){
+      return true;
+    }else {
+      return false;
+    }
+  }
+}
+
 
 function App() {
+  // sort state var
+  const [sort, setSort] = useState("default");
+
+  // filter state var
+  const [type1,setType1] = useState({
+    oil: false,
+    pencil: false,
+    ink: false,
+  });
+  const { oil, pencil, ink } = type1;
+
+  const [type2,setType2] = useState({
+    US: false,
+    France: false,
+    Netherlands: false,
+    Germany: false,
+    Austria: false,
+    UK: false,
+    Unknown: false
+  });
+  const { US, France, Netherlands , Germany, Austria, UK, Unknown} = type2;
+
+  const filteredData = galleryData.filter(item=>matchesType({item,type1,type2}));
+
+  const [count,setCount] = useState(0);
+  const [bookmark,setBookmark] = useState([]);
+  const [tdim,setTdim] = useState(0);
+
+  const handleReset = () => {
+    setSort("default");
+    setType1({
+      oil: false,
+      pencil: false,
+      ink: false,
+    });
+    setType2({
+      US: false,
+      France: false,
+      Netherlands: false,
+      Germany: false,
+      Austria: false,
+      UK: false,
+      Unknown: false
+    });
+    setCount(0);
+    setBookmark([]);
+    setTdim(0);
+ }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <header className="Header">
+          My Gallery
+        </header>
+      </div>
+
+
+      <div className="Body">
+        <div className="Filter">
+          <Sort sort={sort} setSort={setSort}/>
+          {matchesSort({sort})}
+          <Filter1 oil={oil} pencil={pencil} ink ={ink} type1={type1} setType1={setType1}/>
+          <Filter2 US={US} France={France} Netherlands={Netherlands} Germany={Germany} Austria={Austria} UK={UK} Unknown={Unknown} type2={type2} setType2={setType2}/>
+          {/* {count}
+          {bookmark.map((item, index) => (
+              <p>{item}</p>
+            ))} */}
+          <Bookmark count={count} bookmark={bookmark} tdim={tdim}></Bookmark>
+          <Button variant="contained" size="large" onClick={handleReset}>Reset</Button>
+        </div>
+  
+        <div className = "GalleryItems">
+            {filteredData.map((item, index) => (
+              <GalleryItem key={item.name} item={item} bookmark={bookmark} setBookmark={setBookmark} count={count} setCount={setCount} tdim={tdim} setTdim={setTdim}/>
+            ))}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Filter2.js
@@ -0,0 +1,70 @@
+import * as React from 'react';
+import FormLabel from '@mui/material/FormLabel';
+import FormControl from '@mui/material/FormControl';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+
+// Source: https://mui.com/material-ui/react-checkbox/
+export default function Filter2({US,France,Netherlands,Germany,Austria,UK,Unknown,type2,setType2}) {
+
+  const handleChange = (event) => {
+    setType2({
+      ...type2,
+      [event.target.name]: event.target.checked,
+    });
+  };
+
+
+  return (
+    <div>
+      <FormControl sx={{ m: 3 }} component="fieldset" variant="standard">
+        <FormLabel component="legend">Current Location</FormLabel>
+        <FormGroup>
+          <FormControlLabel
+            control={
+              <Checkbox checked={US} onChange={handleChange} name="US" />
+            }
+            label="US"
+          />
+          <FormControlLabel
+            control={
+              <Checkbox checked={France} onChange={handleChange} name="France" />
+            }
+            label="France"
+          />
+          <FormControlLabel
+            control={
+              <Checkbox checked={Netherlands} onChange={handleChange} name="Netherlands" />
+            }
+            label="Netherlands"
+          />
+          <FormControlLabel
+            control={
+              <Checkbox checked={Germany} onChange={handleChange} name="Germany" />
+            }
+            label="Germany"
+          />
+          <FormControlLabel
+            control={
+              <Checkbox checked={Austria} onChange={handleChange} name="Austria" />
+            }
+            label="Austria"
+          />
+          <FormControlLabel
+            control={
+              <Checkbox checked={UK} onChange={handleChange} name="UK" />
+            }
+            label="UK"
+          />
+          <FormControlLabel
+            control={
+              <Checkbox checked={Unknown} onChange={handleChange} name="Unknown" />
+            }
+            label="Unknown"
+          />
+        </FormGroup>
+      </FormControl>
+    </div>
+  );
+}
