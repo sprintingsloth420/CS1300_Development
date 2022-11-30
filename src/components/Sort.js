35d61a3fdc1c1f52e20652e96a4f36fb645fd6a5
yuchen_han+cs1300@brown.edu <yuchen_han+cs1300@brown.edu>
2022-11-30 18:03:03 -0500

    handin

--- src/components/Sort.js
+++ src/components/Sort.js
@@ -8,8 +8,7 @@ import FormLabel from '@mui/material/FormLabel';
 // Source: https://mui.com/material-ui/react-radio-button/
 export default function Sort({sort,setSort}) {
     const handleSort = (event) => {
-        const asort = event.target.value;
-        setSort(asort);
+        setSort(event.target.value);
       };
 
   return (
